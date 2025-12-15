738e2b4f436e61fadd6d8e5c7fbb23443bfe13d0
禾伯 <hebo.pb@alibaba-inc.com>
2025-12-15 14:45:33 +0800

    feat: add LuaMsgpackLibrary

---
+++ Plugins/LuaExtension/Source/ThirdParty/LuaMsgpackLibrary/LuaMsgpackLibrary.Build.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using UnrealBuildTool;
+
+public class LuaMsgpackLibrary : ModuleRules
+{
+	public LuaMsgpackLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory,"lib/Win64/lmsgpack.lib"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib/Mac/liblmsgpack.dylib"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Linux)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib/Linux/liblmsgpack.so"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib/iOS/liblmsgpack.a"));
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib/Android/ARMv7/liblmsgpack.a"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib/Android/ARM64/liblmsgpack.a"));
+		}
+
+		PublicSystemIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+	}
+}
---
+++ Plugins/LuaExtension/Source/ThirdParty/LuaMsgpackLibrary/include/lmsgpack.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+#if defined(_WIN32) || defined(_WINDOWS)
+	#ifdef lmsgpack_EXPORTS
+		#define LMSGPACK_API __declspec(dllexport)
+	#else
+		#define LMSGPACK_API __declspec(dllimport)
+	#endif
+#else
+	#ifdef lmsgpack_EXPORTS
+		#define LMSGPACK_API __attribute__((__visibility__("default")))
+	#else
+		#define LMSGPACK_API extern
+	#endif
+#endif
+
+struct lua_State;
+
+LMSGPACK_API int32_t luaopen_lmsgpack(struct lua_State* L);
